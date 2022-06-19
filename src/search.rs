bf6b7e18d5e721924917cec11ca15bdf27189aaa
Natan Freeman <natanfreeman@outlook.com>
2022-06-19 16:15:33 +0300

    feev: improves documentation

--- src/search.rs
+++ src/search.rs
@@ -6,15 +6,26 @@ use async_std::path::PathBuf;
 use futures::{future::select_ok, TryStreamExt};
 
 #[derive(Clone, Copy)]
+/// A type representing the type of an item.
 pub enum ItemType {
+    ///the item must be a file
     File,
+    ///the item must be a directory
     Directory,
+    ///the item may be a directory or a file
     Either,
 }
-//super fancy async recursion for finding the `data` folder
+/// asynchronously finds the first item (folder or file) in the given directory that matches the given name and type.
+///
+/// `child` is the name+extension of the file or folder to find.
+/// # Errors
+/// An error will be returned in the following situations:
+/// * `path` does not point to an existing directory.
+/// * the `item` does not exist.
+/// * any other IO error.
 #[async_recursion]
-pub async fn find_folder(path: PathBuf, child: String, item_type: ItemType) -> Result<String> {
-    let mut files = async_std::fs::read_dir(path).await?.into_stream();
+pub async fn find_item(dir: PathBuf, child: String, item_type: ItemType) -> Result<String> {
+    let mut files = async_std::fs::read_dir(dir).await?.into_stream();
     //all the found folders in the current directory
     let mut folders = Vec::new();
     while let Some(path) = files.try_next().await? {
@@ -39,7 +50,7 @@ pub async fn find_folder(path: PathBuf, child: String, item_type: ItemType) -> R
         }
         if path.is_dir().await {
             //recursively adds all the folders inside the folder to `folders`
-            folders.push(find_folder(path.clone(), child.clone(), item_type));
+            folders.push(find_item(path.clone(), child.clone(), item_type));
         }
     }
     if folders.is_empty() {
--- src/tests.rs
+++ src/tests.rs
@@ -1,5 +1,5 @@
 #![cfg(test)]
-use crate::search::find_folder;
+use crate::search::find_item;
 use crate::search::ItemType;
 use futures::executor::block_on;
 
@@ -12,13 +12,13 @@ fn compiles() {
 fn runs() {
     let path = std::env::current_dir().unwrap();
 
-    let _data_path = block_on(find_folder(path.into(), "".to_string(), ItemType::Either));
+    let _data_path = block_on(find_item(path.into(), "".to_string(), ItemType::Either));
 }
 #[test]
 fn finds_dir() {
     let path = std::env::current_dir().unwrap();
     //wether in debug or build, cargo should produce a folder called `build`
-    let _data_path = block_on(find_folder(
+    let _data_path = block_on(find_item(
         path.into(),
         "build".to_string(),
         ItemType::Directory,
@@ -30,7 +30,7 @@ fn finds_file() {
     let path = std::env::current_dir().unwrap();
 
     //wether in debug or build, cargo should produce a file called `.cargo-lock`
-    let _data_path = block_on(find_folder(
+    let _data_path = block_on(find_item(
         path.into(),
         ".cargo-lock".to_string(),
         ItemType::File,
@@ -41,7 +41,7 @@ fn finds_file() {
 fn fails_to_find_file() {
     let path = std::env::current_dir().unwrap();
 
-    let result = block_on(find_folder(
+    let result = block_on(find_item(
         path.into(),
         "not_a_file".to_string(),
         ItemType::File,
